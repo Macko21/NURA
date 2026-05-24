de5398f84b30875e467937812a73737fadb9cda7
Macko <84351024+Macko21@users.noreply.github.com>
2026-05-23 21:46:28 -0300

    Add files via upload

---
+++ env.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyCCKZAfclBMli71_50UQv3h4fAUNigvXqA",
+      authDomain: "nura-26bef.firebaseapp.com",
+      databaseURL: "https://nura-26bef-default-rtdb.firebaseio.com",
+      projectId: "nura-26bef",
+      storageBucket: "nura-26bef.firebasestorage.app",
+      messagingSenderId: "500143055377",
+      appId: "1:500143055377:web:23e79056d42007d525c7b0"
+};
